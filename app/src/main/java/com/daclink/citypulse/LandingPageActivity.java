cdb09cd38d6c3c91a4238b99a4ae826305d4ab4b
Tanmay Zende <tanmay7997@gmail.com>
2025-04-16 18:51:39 -0700

    working on the landing page

---
+++ app/src/main/java/com/daclink/citypulse/LandingPageActivity.java
@@ -0,0 +1,12 @@
+package com.daclink.citypulse;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LandingPageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+    }
+}
