5b311a150c0ceedb99bf75b74200b2182fdd398d
Tanmay Zende <tanmay7997@gmail.com>
2025-04-21 23:44:00 -0700

    empty landing activity pushed to save on main, need to copy from landing page branch and implement here

---
+++ app/src/main/java/com/daclink/citypulse/LandingActivity.java
@@ -0,0 +1,15 @@
+package com.daclink.citypulse;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LandingActivity extends AppCompatActivity {
+
+}
